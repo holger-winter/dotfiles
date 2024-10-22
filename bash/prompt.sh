413be77a561fb65499636faca00d3c8cbf958104
Holger Winter <holger.winter@outlook.com>
2024-10-22 14:34:47 +0200

    updated tmux and bash configuration

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,4 +1,31 @@
-# prompt.sh
+# Prompt
 
-PROMPT='[\u@\h \w] '
+### Git Prompt
+
+GIT_DIRTY='*'
+GIT_CLEAN=''
+GIT=$(which git 2>/dev/null)
+
+function parse_git_dirty {
+  if $GIT diff-index --quiet HEAD; then
+    echo -e "$GIT_CLEAN"
+  else
+    echo -e "$GIT_DIRTY"
+  fi
+}
+
+# Make branch and dirty status in prompt
+function git_prompt_info {
+  ref=$($GIT symbolic-ref HEAD 2> /dev/null) || return
+  echo -e " (${ref#refs/heads/}$(parse_git_dirty))"
+}
+
+# Prompt colors and configuration
+# PROMPT='\A \[\e[1;37m\]\u\[\e[0m\]@\[\e[1;32m\]\h\[\e[0m\]:\[\e[34m\]\w\[\e[0m\]\[\e[0m\]$(git_prompt_info) \$\[\e[0m\] '
+PROMPT='\A \[\e[34m\]\w\[\e[0m\]\[\e[0m\]$(git_prompt_info) \$\[\e[0m\] '
 PS1="${PROMPT}"
+
+# For longer commands this is useful sometimes
+miniprompt() {
+  PS1='$ '
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# README
+
+# Prerequesites
+
+- Install Tmux Plugin Manager with `git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm`
+- Run tmux and install plugins with `C-a` and `I` 
+
+# Links
+
+- Vim Keyboard Shortcuts [https://gist.github.com/tuxfight3r/0dca25825d9f2608714b](https://gist.github.com/tuxfight3r/0dca25825d9f2608714b)
