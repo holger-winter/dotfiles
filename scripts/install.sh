46bbc196a296db411bc354143012afe3348f78d1
Holger Winter <holger.winter@outlook.com>
2025-04-23 17:51:03 +0200

    feat: trying something new

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,52 @@
 #!/usr/bin/env bash
 
-# install vim-airline
-git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+function cleanup() {
+  mkdir ~/.backup
+  mv ~/.bashrc ~/.backup
+  mv ~/.gitconfig ~/.backup
+  mv ~/.tmux.conf ~/.backup
+  mv ~/.vimrc ~/.backup
+}
 
-# install vim-airline-themes
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes
+function setupDotfiles() {
+  cd ~/
+  git clone https://github.com/holger-winter/dotfiles.git .config
 
-# install colorscheme solarized
-curl -fLo ~/.vim/colors/solarized.vim --create-dirs https://raw.githubusercontent.com/ericbn/vim-solarized/master/colors/solarized.vim
+  # create local gitconfig
+  touch ~/.gitconfig.local
+}
 
-# clone dotfiles
-git clone ... 
+function setupVim() {
+  # install vim-airline
+  git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
 
-# symlinks
-ln -s ~/.config/bash/.bashrc ~/.bashrc
-ln -s ~/.config/git/.gitconfig ~/.gitconfig
-ln -s ~/.config/tmux/.tmux.conf ~/.tmux.conf
-ln -s ~/.config/vim/.vimrc ~/.vimrc
+  # install vim-airline-themes
+  git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes
+
+  # install solarized
+  curl -fLo ~/.vim/colors/solarized.vim --create-dirs \
+  https://raw.githubusercontent.com/ericbn/vim-solarized/master/colors/solarized.vim
+}
+
+function setupSymlinks() {
+  # symlink for bashrc
+  ln -sf ~/.config/bash/.bashrc ~/.bashrc
+
+  # symlink for gitconfig
+  ln -sf ~/.config/git/.gitconfig ~/.gitconfig
+
+  # symlink for tmux.conf
+  ln -sf ~/.config/tmux/.tmux.conf ~/.tmux.conf
+
+  # symlink for vimrc
+  ln -sf ~/.config/vim/.vimrc ~/.vimrc
+}
+
+function install() {
+  cleanup
+  setupDotfiles
+  setupVim
+  setupSymlinks
+}
+
+install
