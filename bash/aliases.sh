030ebe6bdcea369d9df9f4fe88950f52d3973d90
Holger Winter <holger.winter@outlook.com>
2025-09-24 12:29:40 +0200

    feat: move alias and bash-completion to seperate files

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,4 +2,17 @@ alias c='clear'
 alias h='history'
 alias x='exit'
 
+alias ..='cd ../'
+
+if [ -x /usr/bin/dircolors ]; then
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+  alias ls='ls --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
+fi
+
+alias ll='ls -alF'
+alias la='ls -A'
+
 alias reload='source ~/.bashrc'
